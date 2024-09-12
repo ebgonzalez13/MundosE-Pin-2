f878732ca9b3dd28040271923a17a3b4baec7689
ebgonzalez13 <127209544+ebgonzalez13@users.noreply.github.com>
2024-09-11 21:21:06 -0300

    Update backend.tf

--- backend.tf
+++ backend.tf
@@ -1,6 +1,6 @@
 terraform {
   backend "s3"{
-    bucket                 = "mundose222"
+    bucket                 = "mybucketpin2"
     region                 = "us-east-1"
     key                    = "backend.tfstate"
     dynamodb_table         = "terraformstatelock"
